d78797243d2a11e356cbaffa60f9f81c002f74c4
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2020-10-11 02:10:53 -0300

    Add: dynamoose offline config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import * as dynamoose from 'dynamoose';
+import { routes } from './handlers/image';
+
+const sdk = dynamoose.aws.sdk;
+sdk.config.update({
+  region: process.env.AWS_REGION || 'us-east-2'
+});
+if (process.env.NODE_ENV === 'development') {
+  dynamoose.aws.ddb.local('http://localhost:8001');
+}
 
 const app = express();
 const port = process.env.PORT || '8000';
+
+app.use(routes);
 
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`);
