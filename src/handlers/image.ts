0e964c323eca2d92e719cdfced5200a814162ad9
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2020-10-15 01:39:48 -0300

    Add: requiring authentication on manage images routes

--- src/handlers/image.ts
+++ src/handlers/image.ts
@@ -3,6 +3,7 @@ import passport from 'passport';
 import { Image } from '../models/Image';
 import { User } from '../models/User';
 import { upload } from '../services/image-upload';
+import { isAuthenticated } from '../utils/passport';
 
 const routes = Router();
 const singleUpload = upload.single('image');
@@ -17,39 +18,34 @@ routes.get<{ id: string }>('/:id', async (req, res) => {
   return res.json(image);
 });
 
-routes.post(
-  '/',
-  passport.authenticate('google'),
-  upload.single('image'),
-  async (req, res) => {
-    singleUpload(req, res, async function (err: any) {
-      if (err) {
-        return res.status(422).send({
-          errors: [{ title: 'Image Upload Error', detail: err.message }]
-        });
-      }
-      try {
-        const user = await User.get(req.body.creatorId);
-        const image = await Image.create({
-          title: req.body.title,
-          // @ts-ignore : Problem o @type, attribute location does exist on file
-          url: req.file.location,
-          creator: user
-        });
-        return res.json(image);
-      } catch (error) {
-        console.log(error.stack);
-        return res.status(500).send({
-          errors: [{ title: 'Internal Server Error', detail: error }]
-        });
-      }
-    });
-  }
-);
+routes.post('/', isAuthenticated, async (req, res) => {
+  singleUpload(req, res, async function (err: any) {
+    if (err) {
+      return res.status(422).send({
+        errors: [{ title: 'Image Upload Error', detail: err.message }]
+      });
+    }
+    try {
+      const user = await User.get(req.body.creatorId);
+      const image = await Image.create({
+        title: req.body.title,
+        // @ts-ignore : Problem o @type, attribute location does exist on file
+        url: req.file.location,
+        creator: user
+      });
+      return res.json(image);
+    } catch (error) {
+      console.log(error.stack);
+      return res.status(500).send({
+        errors: [{ title: 'Internal Server Error', detail: error }]
+      });
+    }
+  });
+});
 
 routes.put<{ id: string }, any, typeof User>(
   '/:id',
-  passport.authenticate('google'),
+  isAuthenticated,
   async (req, res) => {
     const id = req.params.id;
 
@@ -60,8 +56,10 @@ routes.put<{ id: string }, any, typeof User>(
   }
 );
 
-routes.delete('/:id', passport.authenticate('google'), async (req, res) => {
-  await Image.delete(req.params.id);
+routes.delete('/:id', isAuthenticated, async (req, res) => {
+  await Image.delete(req.params.id).catch((err) =>
+    res.status(500).send({ messagge: err.message() })
+  );
   return res.json({ msg: 'Deleted' });
 });
 
