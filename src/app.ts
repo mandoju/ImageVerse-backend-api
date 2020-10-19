ca969d6526cfdb370b89cb32c2c496b71b8c8d6c
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2020-10-19 04:53:12 -0300

    Add: throw error when sequelize is not connected

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@ export const AppCreator = () => {
       .then(() => console.log('Connection has been established successfully.'));
   } catch (error) {
     console.error('Unable to connect to the database:', error);
+    throw new Error(error);
   }
   sequelize.sync({ alter: true });
 
